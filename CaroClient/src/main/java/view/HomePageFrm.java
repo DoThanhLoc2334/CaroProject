c398a7dbde294601b0b9bb656a65ee7562b5a2b4
DoThanhLoc2334 <locdothanh233@gmail.com>
2025-10-17 08:23:45 +0700

    update(client): update folder and file after merge

--- CaroClient/src/main/java/view/HomePageFrm.java
+++ CaroClient/src/main/java/view/HomePageFrm.java
@@ -2,8 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package view;
-import model.User;
+package View;
+import Model.User;
 import javax.swing.*;
 /**
  *
